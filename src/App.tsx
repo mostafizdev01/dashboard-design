20be091ce3b723cd39402ba3b4101010dd634107
Mostafiz <mostafiz4372@gmail.com>
2026-01-28 06:08:34 +0600

    dashboard design completed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+
+const App = () => {
+  return (
+    <div>
+      <h2 className=" text-3xl text-center mt-8">This is App Router!</h2>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
